edb48a0f5f7c045169a0acb76ae60eca317197e6
Miikeo <Miguel.A.INCO@gmail.com>
2023-01-09 19:02:41 -0600

    Challenge 0 completed

---
+++ challenges/Reto 0/Reto_0.js
@@ -0,0 +1,17 @@
+
+const fizzbuzz = (maxNum) => {
+
+    for(let i = 1; i <= maxNum; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        }else if (i % 3 === 0) {
+            console.log('fizz');
+        }else if (i % 5 === 0) {
+            console.log('buzz');
+        }else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz(100);
